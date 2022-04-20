549470ea7947e07bdae6a9f9c893c85bd3b276a9
jsshin <jsshin6246@tsinfotech.co.kr>
2022-04-20 10:31:17 +0900

    feat: 자동차 이름 관련 유효성 기능 추가

--- src/main/java/racingcar/Domain/Car.java
+++ src/main/java/racingcar/Domain/Car.java
@@ -1,10 +1,24 @@
 package racingcar.Domain;
 
+import org.junit.platform.commons.util.StringUtils;
+
 public class Car {
+    public static final String WRONG_CART_NAME_OVER = "자동차 이름은 5자 이하만 가능하다.";
+    public static final String WRONG_CART_NAME_EMPTY = "자동차 이름은 공백이 아니여야 합니다.";
     private String name;
 
     public Car(String name) {
+        validation(name);
         this.name = name;
+    }
+
+    private void validation(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(WRONG_CART_NAME_OVER);
+        }
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException(WRONG_CART_NAME_EMPTY);
+        }
     }
 
     public String getName() {
--- src/test/java/racingcar/Domain/CarTest.java
+++ src/test/java/racingcar/Domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.Domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +11,28 @@ public class CarTest {
     @DisplayName("자동차는 이름을가진다.")
     @Test
     void set_car_name() {
-        Car car = new Car("jsshin");
-        assertThat(car.getName()).isEqualTo("jsshin");
+        Car car = new Car("shin");
+        assertThat(car.getName()).isEqualTo("shin");
     }
+
+    @DisplayName("자동차 이름은 5자이상이면 IllegalArgumentException 던진다.")
+    @Test
+    void car_name_lengh() {
+        assertThatThrownBy(() -> {
+            Car car = new Car("jsshin11111");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5자 이하만 가능하다.");
+    }
+
+
+    @DisplayName("자동차 이름은 공백이면 IllegalArgumentException 던진다.")
+    @Test
+    void car_name_empty() {
+        assertThatThrownBy(() -> {
+            Car car = new Car("");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 공백이 아니여야 합니다.");
+    }
+
 
 }
