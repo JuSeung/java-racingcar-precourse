511b8b8cbc8529a569bd3c22abf7c4c8145a06cb
jsshin <jsshin6246@tsinfotech.co.kr>
2022-04-23 14:36:24 +0900

    feat: 입력값 받는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,17 +5,31 @@ import racingcar.Domain.CarNames;
 import racingcar.Domain.Laps;
 
 public class InputView {
-
     public static final String INPUT_NAMES_MESSAGE = "경주할자동차이름(이름은쉼표(,)기준으로구분)";
     public static final String INPUT_LAPS_MESSAGE = "시도할회수";
+    public static final String ERROR = "[ERROR]";
 
-    public CarNames askRacingCarName() {
+    public static CarNames askRacingCarName() {
         System.out.println(INPUT_NAMES_MESSAGE);
-        return new CarNames(Console.readLine());
+        while (true) {
+            try {
+                return new CarNames(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR + e.getMessage());
+                return new CarNames(Console.readLine());
+            }
+        }
     }
 
-    public Laps askLapsNumber() {
+    public static Laps askLapsNumber() {
         System.out.println(INPUT_LAPS_MESSAGE);
-        return new Laps(Console.readLine());
+        while (true) {
+            try {
+                return new Laps(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR + e.getMessage());
+                return new Laps(Console.readLine());
+            }
+        }
     }
 }
